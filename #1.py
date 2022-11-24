c3c813ba065aa36e376f5349c550576fdf3f9d4f
KrzysztofSzczygielski <47943142+KrzysztofSzczygielski@users.noreply.github.com>
2022-11-24 13:32:29 +0100

    Update #1.py

--- #1.py
+++ #1.py
@@ -1,4 +1,4 @@
-# https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
 # Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old. 
 # Note: for this exercise, the expectation is that you explicitly write out the year (and therefore be out of date the next year). If you want to do this in a generic way, see exercise 39.
 
@@ -12,16 +12,19 @@ imie = input ("Podaj swoje imie\n")
 rok = int(input ("Podaj rok swojego urodzenia\n"))
 wiek = int(dzis.strftime("%Y"))- rok
 wiek100 = (rok + 100)
+ilosc_druku = int(input("Wypisz w wartości liczbowej ile razy chesz wypisać tekst \n"))
 
-if wiek >100:
-    print("Ha ha ha bardzo śmieszne ludzie tyle nie żyją")
-elif wiek < 0:
-    print("Chyba jeszcze się nie narodziłeś musimy poczekać")
-elif wiek == 0:
-    print(f"Podziwiam cie {imie} w wieku {wiek} lat już umiesz pisać, sto lat osiągniesz w {wiek100} roku")
-elif wiek == 1:
-    print(f"Podziwiam cie {imie} że w wieku {wiek} roku już umiesz pisać, sto lat osiągniesz w {wiek100} roku")
-elif wiek >= 2 and wiek < 5:
-    print(f"Podziwiam cie {imie} że w wieku {wiek} lat już umiesz pisać, sto lat osiągniesz w {wiek100} roku")
-else:
-    print(f"Cześć {imie} widzę że masz {wiek} lat, sto lat osiągniesz w {wiek100} roku")
+for i in range (1,ilosc_druku+1):
+    if wiek >100:
+        print("Ha ha ha bardzo śmieszne ludzie tyle nie żyją")
+    elif wiek < 0:
+        print("Chyba jeszcze się nie narodziłeś musimy poczekać")
+    elif wiek == 0:
+        print(f"{i}: Podziwiam cie {imie} w wieku {wiek} lat już umiesz pisać, sto lat osiągniesz w {wiek100} roku")
+    elif wiek == 1:
+        print(f"{i}: Podziwiam cie {imie} że w wieku {wiek} roku już umiesz pisać, sto lat osiągniesz w {wiek100} roku")
+    elif wiek >= 2 and wiek < 5:
+        print(f"{i}: Podziwiam cie {imie} że w wieku {wiek} lat już umiesz pisać, sto lat osiągniesz w {wiek100} roku")
+    else:
+        print(f"{i}: Cześć {imie} widzę że masz {wiek} lat, sto lat osiągniesz w {wiek100} roku")
+    i=+1
